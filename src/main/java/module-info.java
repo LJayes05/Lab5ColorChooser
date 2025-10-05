4750a50e047eec09ab053a519b4774bda66ea420
LJayes05 <ludyjayes3324@gmail.com>
2025-10-05 19:20:45 -0400

    First commit on Color Chooser project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module ludy.lab5colorchooser {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens ludy.lab5colorchooser to javafx.fxml;
+    exports ludy.lab5colorchooser;
+}
